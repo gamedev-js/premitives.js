a20f33fa22f3203e7d39ed9f8e187c737515b991
shay_chen <641278529@qq.com>
2018-01-08 10:28:49 +0800

    Modify indent error

--- examples/src/capsule.js
+++ examples/src/capsule.js
@@ -1,6 +1,6 @@
 (() => {
-    return window.primitives.capsule(0.5, 0.5, 2,{
-        heightSegments: 30,
-        sides:20,
-    });
+  return window.primitives.capsule(0.5, 0.5, 2, {
+    heightSegments: 30,
+    sides: 20,
+  });
 })();
